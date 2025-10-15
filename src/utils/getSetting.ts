fd81da763032b73ed91769302ddd55352c445b59
Nelson Job <njobnz@proton.me>
2025-10-16 08:37:56 +1300

    fix: correct fallback check for global settings retrieval

--- src/utils/getSetting.ts
+++ src/utils/getSetting.ts
@@ -3,7 +3,7 @@ import joplin from 'api';
 export default async (name: string, fallback: any = null, isGlobal: boolean = false): Promise<any> => {
   try {
     if (isGlobal) {
-      return joplin.settings.globalValues === undefined
+      return joplin.settings.globalValues !== undefined
         ? (await joplin.settings.globalValues([name]))[0]
         : await joplin.settings.globalValue(name);
     }
